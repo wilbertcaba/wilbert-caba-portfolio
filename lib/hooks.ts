32a926a0163a95d9cd17a7350d16f7dfb51bbebb
Wilbert Caba <wilbertcaba@gmail.com>
2024-02-26 00:08:36 -0500

    Created custom hook for navigation bar active state

---
+++ lib/hooks.ts
@@ -0,0 +1,26 @@
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import type { SectionName } from "./types";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+    const { ref, inView } = useInView({
+        threshold,
+    });
+    const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+    // This conditional is necessary to prevent the active section from changing
+    // when the user clicks on a link to navigate to a section.
+    // Also prevents the observer from setting the active section when the user
+    // scrolls to a section.
+    useEffect(() => {
+        if (inView && Date.now() - timeOfLastClick > 1000) {
+            setActiveSection(sectionName);
+            console.log("Setting active section to", sectionName);
+        }
+    }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+    return {
+        ref,
+    };
+}
