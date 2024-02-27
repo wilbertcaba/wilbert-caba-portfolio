c6bfb8a0c472007c67f9881166c3d1d3f053a90b
Wilbert Caba <wilbertcaba@gmail.com>
2024-02-26 23:06:34 -0500

    Skills section implemented

--- components/projects.tsx
+++ components/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
     const { ref } = useSectionInView("Projects", 0.3);
 
     return (
-        <section ref={ref} className="scroll-mt-28" id="projects">
+        <section ref={ref} className="scroll-mt-28 mb-28" id="projects">
             <SectionHeading>My Projects</SectionHeading>
             <div>
                 {projectsData.map((project, index) => (
