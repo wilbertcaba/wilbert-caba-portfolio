42a58d814128644ec607547cfe94e8ca8582f220
Wilbert Caba <wilbertcaba@gmail.com>
2024-02-28 13:43:28 -0500

    Created Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import About from "@/components/about";
 import SectionDivider from "@/components/section-divider";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
+import Experience from "@/components/experience";
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
             <About />
             <Projects />
             <Skills />
+            <Experience />
         </main>
     );
 }
