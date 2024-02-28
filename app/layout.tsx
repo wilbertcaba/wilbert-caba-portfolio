2c8c48b66797c0b401122dd5c4e7e881cd4c8b17
Wilbert Caba <wilbertcaba@gmail.com>
2024-02-28 13:49:02 -0500

    Removed height helper on Body

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,16 +11,10 @@ export const metadata = {
         "Wilbert Caba is a UX Developer with 17 years of combined experience on UX Design and Front-End Development",
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="!scroll-smooth">
-            <body
-                className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
-            >
+            <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
                 <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
                 <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
                 <ActiveSectionContextProvider>
