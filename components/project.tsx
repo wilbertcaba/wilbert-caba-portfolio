8d85585f4aeab4b628b457f73dcdff50fc76503d
Wilbert Caba <wilbertcaba@gmail.com>
2024-03-04 00:51:04 -0500

    Made Experience and Intro sections responsive

--- components/project.tsx
+++ components/project.tsx
@@ -27,7 +27,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
             className="group mb-3 sm:mb-8 last:mb-0"
         >
             <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden relative sm:h-[20rem] hover:bg-gray-200 transition rounded-lg dark:text-white dark:bg-white/10 dark:hover:bg-white/5">
-                <div className="py-4 px-5 sm:px-10 sm:py-10 sm:max-w-[60%] flex flex-col gap-2 h-full group-even:ml-[18rem] group-odd:mr-[18rem]">
+                <div className="py-4 px-5 sm:px-10 sm:py-10 sm:max-w-[60%] flex flex-col gap-2 h-full sm:group-even:ml-[18rem] sm:group-odd:mr-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
                     <p className="leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
                     <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -49,16 +49,13 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
                     transition
                     group-even:right-[initial]
                     group-even:-left-44 
-
                     group-hover:-translate-x-3 
                     group-hover:translate-y-3 
                     group-hover:-rotate-2 
-
                     group-even:group-hover:translate-x-3 
-
                     group-even:group-hover:rotate-2 
-
                     group-hover:scale-[1.04]
+                    hidden sm:block
                     "
                 />
             </section>
