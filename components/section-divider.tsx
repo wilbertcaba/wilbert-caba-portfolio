28d60e29726976bf83b0c8bada74fb6218f11758
Wilbert Caba <wilbertcaba@gmail.com>
2024-03-03 23:26:04 -0500

    Added dark mode classes to most components

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import React from 'react'
-import { delay, motion } from 'framer-motion';
+import React from "react";
+import { delay, motion } from "framer-motion";
 
 export default function SectionDivider() {
-  return (
-    <motion.div 
-        className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
-        initial={{ y: 0, scale: 0, opacity: 0 }}
-        animate={{ y: 0, scale: 1, opacity: 1 }}
-        transition={{delay: 0.1}}
-    >
-    </motion.div>
-  )
+    return (
+        <motion.div
+            className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
+            initial={{ y: 0, scale: 0, opacity: 0 }}
+            animate={{ y: 0, scale: 1, opacity: 1 }}
+            transition={{ delay: 0.1 }}
+        ></motion.div>
+    );
 }
