69e505a653f7aa8337c8f890afc2b421103d341a
Wilbert Caba <wilbertcaba@gmail.com>
2024-03-04 00:08:23 -0500

    Created ThemeContextProvider and useTheme hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,13 +23,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             >
                 <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
                 <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-                <ActiveSectionContextProvider>
-                    <Header />
-                    {children}
-                    <Toaster position="top-center" />
-                    <Footer />
-                </ActiveSectionContextProvider>
-                <ThemeSwitch />
+
+                <ThemeContextProvider>
+                    <ActiveSectionContextProvider>
+                        <Header />
+                        {children}
+                        <Toaster position="top-center" />
+                        <Footer />
+                    </ActiveSectionContextProvider>
+
+                    <ThemeSwitch />
+                </ThemeContextProvider>
             </body>
         </html>
     );
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,38 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useTheme } from "@/context/theme-context";
 import { BsMoon, BsSun } from "react-icons/bs";
 
-type Theme = "light" | "dark";
-
 export default function ThemeSwitch() {
-    const [theme, setTheme] = useState<Theme>("light");
-
-    const toggleTheme = () => {
-        if (theme === "light") {
-            setTheme("dark");
-            document.documentElement.classList.add("dark");
-            window.localStorage.setItem("theme", "dark");
-        } else {
-            setTheme("light");
-            window.localStorage.setItem("theme", "light");
-            document.documentElement.classList.remove("dark");
-        }
-    };
-
-    useEffect(() => {
-        const localTheme = window.localStorage.getItem("theme") as Theme | null;
-
-        if (localTheme) {
-            setTheme(localTheme);
-            if (localTheme === "dark") {
-                document.documentElement.classList.add("dark");
-            }
-        } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            setTheme("dark");
-            document.documentElement.classList.add("dark");
-        }
-    }, []);
+    const { theme, toggleTheme } = useTheme();
 
     return (
         <button
