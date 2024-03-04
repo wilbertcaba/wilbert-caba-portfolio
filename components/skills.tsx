c22268073466161d60ef0116470474086118b27d
Wilbert Caba <wilbertcaba@gmail.com>
2024-03-04 01:05:37 -0500

    Added id to Skills component for anchor link

--- components/skills.tsx
+++ components/skills.tsx
@@ -24,7 +24,7 @@ export default function Skills() {
     const { ref } = useSectionInView("Skills");
 
     return (
-        <section ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
+        <section id="skills" ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
             <SectionHeading>My Skills</SectionHeading>
 
             <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
