2203c9494bbd0c858b29f35d418e082d37c63ae8
Wilbert Caba <wilbertcaba@gmail.com>
2024-03-18 00:32:31 -0400

    Root layout and page: include navigation only on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,31 @@ import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
+import ThemeContextProvider from "@/context/theme-context";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import Header from "@/components/header";
+import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center px-4">
-            <Intro />
-            <SectionDivider />
-            <About />
-            <Projects />
-            <Skills />
-            <Experience />
-            <Contact />
+        <main className="flex flex-col items-center px-4 relative pt-28 sm:pt-36">
+            <ThemeContextProvider>
+                <ActiveSectionContextProvider>
+                    <Header />
+                    <Intro />
+                    <SectionDivider />
+                    <About />
+                    <Projects />
+                    <Skills />
+                    <Experience />
+                    <Contact />
+                    <Toaster position="top-center" />
+                    <Footer />
+                </ActiveSectionContextProvider>
+                <ThemeSwitch />
+            </ThemeContextProvider>
         </main>
     );
 }
