eb8ab1644b11fdf8fbd5dbdb661b4baab4355cb5
Wilbert Caba <wilbertcaba@gmail.com>
2024-05-15 23:32:31 -0400

    Testing RSC to fetch data directly with Payload

--- src/components/testComponent.tsx
+++ src/components/testComponent.tsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import config from "@payload-config";
 
-export default function TestComponent() {
-  return <div className="bg-green-500 p-4 text-green-100">Hey! This is a custom component.</div>
+export default async function TestComponent() {
+    const payload = await getPayloadHMR({ config });
+
+    const users = await payload.find({
+        collection: "users",
+    });
+
+    return (
+        <div className="bg-green-500 p-4 text-green-100">
+            Hey! This is a custom component.
+            <h3>Users:</h3>
+            <div className="flex gap-2">
+                {users.docs.map((user) => {
+                    return (
+                        <div key={user.id} className="bg-green-700 p-2 rounded">
+                            <p className="m-0">{user.email as string}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
