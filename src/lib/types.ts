db4fbfd93b7b4061d9c1ff8e2fa10053905e406a
Wilbert Caba <wilbertcaba@gmail.com>
2024-05-28 23:20:20 -0400

    Refacrored types for Project props

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,18 @@
 import { links } from "./data";
-import { projectsData } from "@/lib/data";
+// import { projectsData } from "@/lib/data";
 
 export type SectionName = (typeof links)[number]["name"];
-export type ProjectProps = (typeof projectsData)[number];
+// export type ProjectProps = (typeof projectsData)[number];
+
+export type ProjectProps = {
+    id: number;
+    title: string;
+    excerpt: string;
+    projectLink: string;
+    role: string;
+    tags: string[];
+    // tags: string[];
+    imageUrl: string;
+    slug: string;
+    description_html: string;
+};
