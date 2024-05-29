48c650659af0daa1349a3fdba86ea2a6a5ea242a
Wilbert Caba <wilbertcaba@gmail.com>
2024-05-28 23:25:50 -0400

    Updated Projects component to fetch data from API

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SectionHeading from "./section-heading";
-import { projectsData } from "@/lib/data";
+// import { projectsData } from "@/lib/data";
 import Project from "./project";
-
-import { useScroll } from "framer-motion";
-
 import { useSectionInView } from "@/lib/hooks";
+import { ProjectProps } from "@/lib/types";
 
-export default function Projects() {
+export default function Projects(props: { id: number }) {
     const { ref } = useSectionInView("Projects", 0.3);
+
+    const [project, setProject] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/projects/`)
+            .then((res) => res.json())
+            .then((data) => {
+                setProject(data);
+                console.log(project);
+            });
+    }, []);
 
     return (
         <section ref={ref} className="scroll-mt-28 mb-28" id="projects">
             <SectionHeading>My Projects</SectionHeading>
             <div>
-                {projectsData.map((project, index) => (
+                {project.map((project: ProjectProps, index) => (
                     <React.Fragment key={index}>
                         <Project {...project} />
                     </React.Fragment>
