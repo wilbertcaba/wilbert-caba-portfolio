cc9294b983c73de1253e08cc68853af344d055fe
Wilbert Caba <wilbertcaba@gmail.com>
2024-05-28 23:23:34 -0400

    Created GET API route to fetch projects data

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,7 @@
+import { sql } from "@vercel/postgres";
+
+export async function GET({ params }: { params: { id: number } }) {
+    // const projectsData = await sql`SELECT * FROM projects WHERE id = ${params.id}`;
+    const projectsData = await sql`SELECT * FROM projects`;
+    return Response.json(projectsData.rows);
+}
