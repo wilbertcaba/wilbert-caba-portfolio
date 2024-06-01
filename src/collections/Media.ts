fabc0ae9c444eecb17b54e21a0cf549a56d2f544
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-01 03:37:30 -0400

    Deleted deprecated option staticURL

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -3,7 +3,6 @@ import { CollectionConfig } from "payload/types";
 export const Media: CollectionConfig = {
     slug: "media",
     upload: {
-        // staticURL: "/media",
         staticDir: "media",
         imageSizes: [
             {
