4f5fd6a18e1505f2170377f58635478552f0760c
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-02 17:12:37 -0400

    Created Homepage global

---
+++ src/globals/Homepage.ts
@@ -0,0 +1,46 @@
+import { HTMLConverterFeature, lexicalEditor, lexicalHTML } from "@payloadcms/richtext-lexical";
+import { GlobalConfig } from "payload/types";
+
+export const Homepage: GlobalConfig = {
+    slug: "homepage",
+
+    fields: [
+        {
+            name: "main_value_propostiion",
+            label: "Main Value Proposition",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "about",
+            label: "About",
+            type: "richText",
+            required: true,
+            editor: lexicalEditor({
+                features: ({ defaultFeatures }) => [...defaultFeatures, HTMLConverterFeature({})],
+            }),
+        },
+        lexicalHTML("about", { name: "about_html" }),
+        {
+            name: "resume",
+            label: "Resume",
+            type: "upload",
+            relationTo: "media",
+            admin: {
+                position: "sidebar",
+            },
+        },
+        {
+            name: "skills",
+            label: "Skills",
+            type: "array",
+            fields: [
+                {
+                    name: "skill",
+                    type: "text",
+                },
+            ],
+            required: true,
+        },
+    ],
+};
