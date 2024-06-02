46d9d03d1d777dd506a09f66df5315a4af86dba1
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-02 17:12:57 -0400

    Created Navigation global

---
+++ src/globals/MainNav.ts
@@ -0,0 +1,30 @@
+import { GlobalConfig } from "payload/types";
+
+export const MainNav: GlobalConfig = {
+    slug: "main-navigation",
+
+    fields: [
+        {
+            name: "navigation",
+            type: "array",
+            labels: {
+                singular: "Navigation Item",
+                plural: "Navigation Items",
+            },
+            fields: [
+                {
+                    name: "label",
+                    label: "Label",
+                    type: "text",
+                    required: true,
+                },
+                {
+                    name: "link",
+                    label: "Link",
+                    type: "text",
+                    required: true,
+                },
+            ],
+        },
+    ],
+};
