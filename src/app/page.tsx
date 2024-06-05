550a6b749ec9c27f855636251dfc8441484265a7
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-04 21:11:01 -0400

    refactor: Remove unused id props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,8 @@ import Header from "@/components/header";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
-// import ProjectTest from "@/components/project-test";
 
-export default function Home({ id }: { id: number }) {
+export default function Home() {
     return (
         <main className="flex flex-col items-center px-4 relative pt-28 sm:pt-36">
             <ThemeContextProvider>
@@ -22,10 +21,9 @@ export default function Home({ id }: { id: number }) {
                     <Intro />
                     <SectionDivider />
                     <About />
-                    <Projects id={id} />
+                    <Projects />
                     <Skills />
                     <Experience />
-                    {/* <ProjectTest /> */}
                     <Contact />
                     <Toaster position="top-center" />
                     <Footer />
