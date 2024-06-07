0571cc4a9bcb5ed615dae7b1f6358f5cfbe015fe
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-07 03:09:20 -0400

    Deleted unused API routes

--- src/app/api/projects/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-export async function GET({ params }: { params: { id: number } }) {
-    // const projectsData = await sql`SELECT * FROM projects WHERE id = ${params.id}`;
-    const projectsData = await sql`SELECT * FROM projects WHERE id = ${params.id}`;
-    return Response.json(projectsData.rows);
-}
