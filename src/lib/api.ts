b878827115d03c1ab996cc8b7f66c39645de673c
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-12 01:20:02 -0400

    refactor: Update fetchProjects function to use HTTPS for API URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,7 +8,7 @@ export async function fetchProjects(): Promise<ProjectProps[]> {
     const baseUrl = process.env.VERCEL_URL || process.env.BASE_URL;
     try {
         // await delay(5000);
-        const response = await fetch(`${baseUrl}/api/Projects`);
+        const response = await fetch(`https://${baseUrl}/api/Projects`);
 
         if (!response.ok) {
             console.log(`Base URL: ${baseUrl}`);
