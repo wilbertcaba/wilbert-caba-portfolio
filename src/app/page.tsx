a666fcd09afb499df2f5c536e103480516fe5c4f
Wilbert Caba <wilbertcaba@gmail.com>
2024-06-18 01:28:47 -0400

    feat: Update page component to fetch homepage data and skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,23 +11,25 @@ import Header from "@/components/header";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
-import { fetchExperiences, fetchProjects } from "@/lib/supabase/data";
+import { fetchExperiences, fetchProjects, fetcHomepageData, fetchSkills } from "@/lib/supabase/data";
 import { Suspense } from "react";
 
 export default async function Home() {
     const projects = await fetchProjects();
     const experiences = await fetchExperiences();
+    const homepageData = await fetcHomepageData();
+    const skills = await fetchSkills();
 
     return (
         <main className="flex flex-col items-center px-4 relative pt-28 sm:pt-36">
             <ThemeContextProvider>
                 <ActiveSectionContextProvider>
                     <Header />
-                    <Intro />
+                    <Intro data={homepageData} />
                     <SectionDivider />
-                    <About />
+                    <About data={homepageData} />
                     <Projects data={projects} />
-                    <Skills />
+                    <Skills data={skills} />
                     <Experience data={experiences} />
                     <Contact />
                     <Toaster position="top-center" />
