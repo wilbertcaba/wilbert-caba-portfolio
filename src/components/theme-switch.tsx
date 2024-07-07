62b4973c6924163cd66454d2be46fea9fa9de4f6
Wilbert Caba <wilbertcaba@gmail.com>
2024-07-06 23:29:36 -0400

    refactor: shrank theme switch on mobile

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -8,7 +8,11 @@ export default function ThemeSwitch() {
 
     return (
         <button
-            className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border boder-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.1] active:scale-[1.1] transition-all dark:bg-gray-950"
+            className="
+                fixed top-3 sm:top-auto sm:bottom-5 right-5 bg-white w-[2rem] h-[2rem] sm:w-[3rem] sm:h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] 
+                border boder-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center 
+                hover:scale-[1.1] active:scale-[1.1] transition-all dark:bg-gray-950 z-[120]
+            "
             onClick={toggleTheme}
         >
             {theme === "light" ? <BsSun /> : <BsMoon />}
