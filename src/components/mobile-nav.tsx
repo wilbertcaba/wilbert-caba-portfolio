6fe55fe5331dcfc5382b1e6cd765c0a01db6c4dd
Wilbert Caba <wilbertcaba@gmail.com>
2024-07-07 20:32:05 -0400

    refactor: sheet close button changes its state

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 import { links } from "@/lib/data";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
 import Image from "next/image";
 import { HomepageProps } from "@/lib/types";
 import Link from "next/link";
 import { FiMenu, FiBox, FiBriefcase, FiMail } from "react-icons/fi";
+import { X } from "lucide-react";
 
 export default function MobileNav({ logo }: { logo: HomepageProps["logo"] }) {
     const [isSheetOpen, setSheetOpen] = useState(false);
@@ -39,6 +41,13 @@ export default function MobileNav({ logo }: { logo: HomepageProps["logo"] }) {
                                 <div className="px-4 py-[2.6rem] border-b border-b-gray-900">
                                     <Image src={logo.url} alt={logo.alt} width={200} height={20} />
                                 </div>
+                                <SheetPrimitive.Close
+                                    onClick={closeSheet}
+                                    className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800"
+                                >
+                                    <X className="h-4 w-4" />
+                                    <span className="sr-only">Close</span>
+                                </SheetPrimitive.Close>
                             </SheetHeader>
                             <ul>
                                 {links.map((link) => (
