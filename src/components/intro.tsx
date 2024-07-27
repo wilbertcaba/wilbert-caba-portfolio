75bf5ca017e108b7327ac3fa29ead4f4b53a4332
Wilbert Caba <wilbertcaba@gmail.com>
2024-07-27 18:36:45 -0400

    refactor: Hero section desktop ready

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,12 +5,36 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaBehance, FaDribbble, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { HomepageProps } from "@/lib/types";
 import SectionDivider from "./section-divider";
+import { Button } from "@/components/ui/button";
+
+const social = [
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/wilbert-caba/",
+        icon: <FaLinkedin />,
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com/wilbertcaba",
+        icon: <FaGithub />,
+    },
+    {
+        name: "Dribbble",
+        url: "https://dribbble.com/wilbertcaba",
+        icon: <FaDribbble />,
+    },
+    {
+        name: "Behance",
+        url: "https://www.behance.net/wilbertcaba",
+        icon: <FaBehance />,
+    },
+];
 
 export default function Intro({ h1 }: { h1: HomepageProps["main_value_proposition"] }) {
     const { ref } = useSectionInView("Home", 0.99);
@@ -38,62 +62,40 @@ export default function Intro({ h1 }: { h1: HomepageProps["main_value_propositio
                 <SectionDivider />
 
                 <motion.div
-                    className="flex flex-col gap-12 justify-center text-lg font-medium"
+                    className="flex flex-col gap-12 align-center text-lg font-medium"
                     initial={{ y: 100, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                     transition={{
                         delay: 0.1,
                     }}
                 >
-                    <div className="flex gap-8">
-                        <Link
-                            href="#contact"
-                            className="group bg-gray-800 text-white shadow-md px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none hover:bg-gray-950 transition duration-[0.3s]"
-                            onClick={() => (setActiveSection("Contact"), setTimeOfLastClick(Date.now()))}
-                        >
-                            Contact Me
-                            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition duration-[0.3s]" />
-                        </Link>
-                        <a
-                            href="/resume.pdf"
-                            download
-                            className="group bg-white shadow-md px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none hover:shadow-none transition duration-[0.3s] dark:bg-white/10"
-                        >
-                            Download CV
-                            <HiDownload className="opacity-70 group-hover:translate-y-1 group-hover:opacity-1 transition duration-[0.3s]" />
-                        </a>
+                    <div className="flex justify-center gap-8">
+                        <Button asChild>
+                            <Link
+                                href="#contact"
+                                className="group gap-2 hover:bg-gray-950 min-w-44"
+                                onClick={() => (setActiveSection("Projects"), setTimeOfLastClick(Date.now()))}
+                            >
+                                Contact Me
+                                <BsArrowRight className="group-hover:translate-x-1" />
+                            </Link>
+                        </Button>
+
+                        <Button asChild variant="outline">
+                            <a href="/resume.pdf" download className="group gap-2 min-w-44">
+                                Download CV
+                                <HiDownload className="group-hover:translate-y-1" />
+                            </a>
+                        </Button>
                     </div>
                     <div className="flex gap-8 justify-center">
-                        <div className="flex items-center gap-8 justify-center mt-12 sm:mt-0">
-                            <a
-                                href="https://www.linkedin.com/in/wilbert-caba/"
-                                target="_blank"
-                                className="bg-white shadow-md w-12 h-12 flex items-center justify-center gap-2 rounded-full outline-none hover:shadow-none transition duration-[0.3s] dark:bg-white/10 dark:text-white/60"
-                            >
-                                <BsLinkedin />
-                            </a>
-                            <a
-                                href="https://github.com/wilbertcaba"
-                                target="_blank"
-                                className="bg-white shadow-md w-12 h-12 flex items-center justify-center gap-2 rounded-full text-[1.25rem] outline-none hover:shadow-none transition duration-[0.3s] dark:bg-white/10 dark:text-white/60"
-                            >
-                                <FaGithubSquare />
-                            </a>
-                            <a
-                                href="https://www.linkedin.com/in/wilbert-caba/"
-                                target="_blank"
-                                className="bg-white shadow-md w-12 h-12 flex items-center justify-center gap-2 rounded-full outline-none hover:shadow-none transition duration-[0.3s] dark:bg-white/10 dark:text-white/60"
-                            >
-                                <BsLinkedin />
-                            </a>
-                            <a
-                                href="https://github.com/wilbertcaba"
-                                target="_blank"
-                                className="bg-white shadow-md w-12 h-12 flex items-center justify-center gap-2 rounded-full text-[1.25rem] outline-none hover:shadow-none transition duration-[0.3s] dark:bg-white/10 dark:text-white/60"
-                            >
-                                <FaGithubSquare />
-                            </a>
-                        </div>
+                        {social.map((item, i) => (
+                            <Button asChild variant="secondary" key={i}>
+                                <a href={item.url} target="_blank" className="w-12 h-12">
+                                    {item.icon}
+                                </a>
+                            </Button>
+                        ))}
                     </div>
                 </motion.div>
             </div>
