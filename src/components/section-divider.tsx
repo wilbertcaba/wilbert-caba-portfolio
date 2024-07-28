a565e6aafbd8f7480392240732d3f9dc7ab7bef3
Wilbert Caba <wilbertcaba@gmail.com>
2024-07-27 22:39:31 -0400

    refactor: Included section divider on mobile

--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -6,7 +6,7 @@ import { delay, motion } from "framer-motion";
 export default function SectionDivider() {
     return (
         <motion.div
-            className="bg-primary my-12 h-16 w-1 rounded-full hidden sm:block"
+            className="bg-primary my-12 h-16 w-1 rounded-full"
             initial={{ y: 0, scale: 0, opacity: 0 }}
             animate={{ y: 0, scale: 1, opacity: 1 }}
             transition={{ delay: 0.1 }}
