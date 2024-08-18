43a2be08cbffbff87a35c081727ac59344e0de52
Wilbert Caba <wilbertcaba@gmail.com>
2024-08-18 13:12:24 -0400

    refactor: Update new Payload import pattern for custom components

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,5 @@
-import TestComponent from "@/components/testComponent";
-import type { CollectionConfig } from "payload/types";
+// import TestComponent from "@/components/testComponent";
+import type { CollectionConfig } from "payload";
 
 export const Users: CollectionConfig = {
     slug: "users",
@@ -15,7 +15,10 @@ export const Users: CollectionConfig = {
             type: "ui",
             admin: {
                 components: {
-                    Field: TestComponent,
+                    // Check v3.0.0-beta.56 for breaking changes with fields
+                    // Refactored using the new custom component import method:
+                    // https://payloadcms.com/docs/beta/admin/components#defining-custom-components-in-the-payload-config
+                    Field: "/src/components/testComponent.tsx",
                 },
             },
         },
